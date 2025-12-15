4f67e8bc4f2af96f1682315fe5d0955c2b94b6d9
swk <sewang1005@gmail.com>
2025-12-15 15:56:29 +0900

    Day3: 평균이상 + 나이순 정렬

--- main.py
+++ main.py
@@ -9,12 +9,21 @@ def read_csv(filename):
             row["score"] = int(row["score"])
             data.append(row)
     return data
+#평균점수계산
 def average_score(data):
     return sum(p["score"] for p in data) / len(data)
 
-
+#조건 필터링
 def filter_scores(data,threshold=85):
     return [p for p in data if p["score"]>=threshold]
+#평균 이상 학생
+def above_average(data, avg):
+    return [p for p in data if p["score"]>avg]
+
+#나이순 정렬
+def sort_by_age(data):
+    return sorted(data, key=lambda x: x["age"])
+
 
 def save_csv(data, filename):
     with open(filename, "w", newline = '', encoding = "utf-8") as f:
@@ -26,16 +35,29 @@ def save_csv(data, filename):
 def main():
     data = read_csv("data.csv")
     print("전체 데이터: ", data)
+
     avg = average_score(data)
     print("전체평균점수:",avg)
+
     high_scores = filter_scores(data,85)
     print("\n점수 85이상 학생: ")
+
     for p in high_scores:
         print(p["name"], p["score"])
     save_csv(high_scores, "high_scores.csv")
-    above_avg = [p for p in data if p["score"]>avg]
-    sorted_by_age = sorted(data, key=lambda x: x["age"])
-    print("\nhigh_scores.csv 파일 생성완료")
+    
+    above_avg_students= above_average(data,avg)
+    print("\n평균 이상 학생")
+    for p in above_avg_students:
+        print(p["name"], p["score"])
+    save_csv(above_avg_students, "above_avg.csv")
+
+    sorted_students = sort_by_age(data)
+    print("\n나이순 정렬")
+    for p in sorted_students:
+        print(p["name"], p["age"], p["score"])
+    
+    print("\ncsv 파일 생성완료")
 if __name__ == "__main__":
     main()
 
